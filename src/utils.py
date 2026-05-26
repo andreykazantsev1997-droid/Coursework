1e13de645090e143c109fb52b4b0024c174cee43
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-05-26 20:29:01 +0300

    add and update func

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,27 @@
+import json
 import os
+from typing import Any, cast
+
 import pandas as pd
 
-def load_excel(file_path="../data/operations.xlsx"):
+
+def load_excel(file_path: str = "../data/operations.xlsx") -> list[Any]:
     """Функция, которая читает Excel-файл и возвращает список словарей"""
     if not os.path.exists(file_path):
         print(f"Критическая ошибка: Файл {file_path} не найден")
         return []
     try:
         data_frame = pd.read_excel(file_path)
-        data_frame = data_frame.where(pd.notnull(data_frame), None)
+        data_frame = data_frame.replace({pd.NA: None, float("nan"): None})
         return data_frame.to_dict(orient="records")
     except Exception as e:
         print(f"Не удалось открыть Excel-файл: {e}")
         return []
+
+
+def load_user_settings(file_path: str) -> dict[str, Any]:
+    """Функция, которая загружает список валют и акций из настроек пользователя"""
+    if not os.path.exists(file_path):
+        return {}
+    with open(file_path, "r", encoding="utf-8") as file:
+        return cast(dict[str, Any], json.load(file))
